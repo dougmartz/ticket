8c22407224b9fb6efe8c7e6d5ae207d1a5137c66
Waan <admin@waan.email>
2019-08-17 21:16:36 +0530

    update

---
+++ src/main/java/com/ticket/main/Ticket.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ticket.main;
+
+/**
+ *
+ * @author admin
+ */
+public class Ticket {
+    
+    public static void main (String [] args){
+    
+        System.out.println("Hello World");
+    }
+}
