8fe04ef67a27aa8a7392b901c2bff7c7a6fc1bf6
dougmartz <54201707+dougmartz@users.noreply.github.com>
2019-08-18 12:08:03 -0400

    Add files via upload

--- src/main/java/com/ticket/main/Ticket.java
+++ src/main/java/com/ticket/main/Ticket.java
@@ -5,6 +5,14 @@
  */
 package com.ticket.main;
 
+import com.ticket.database.Connect;
+import com.ticket.models.LoginModel;
+import com.ticket.views.Login;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 /**
  *
  * @author admin
@@ -12,7 +20,16 @@ package com.ticket.main;
 public class Ticket {
     
     public static void main (String [] args){
-    
-        System.out.println("Hello World");
+        new Connect();
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            java.awt.EventQueue.invokeLater(() -> {
+                new Login();
+            });
+        } catch (ClassNotFoundException | InstantiationException 
+                | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println("Running");
     }
 }
