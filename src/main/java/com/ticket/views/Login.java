7feedbc2eb220f97bf8882ac9c40545351f2a4a6
dougmartz <54201707+dougmartz@users.noreply.github.com>
2019-08-18 13:18:00 -0400

    Update Login.java

--- src/main/java/com/ticket/views/Login.java
+++ src/main/java/com/ticket/views/Login.java
@@ -1,3 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ticket.views;
+
+import com.ticket.models.LoginModel;
+import java.awt.Dimension;
 import java.awt.Font;
 import javax.swing.JButton;
 import javax.swing.JFrame;
