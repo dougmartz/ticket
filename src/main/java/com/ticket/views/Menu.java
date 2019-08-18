4dae24f3ab5ba971b76e1580b2773da2bfb35041
dougmartz <54201707+dougmartz@users.noreply.github.com>
2019-08-18 12:07:00 -0400

    Add files via upload

---
+++ src/main/java/com/ticket/views/Menu.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ticket.views;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Douglas
+ */
+public class Menu extends JFrame{
+
+    public Menu() {
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        this.setLocationRelativeTo(null) ;
+        this.setVisible(true);
+    }
+    
+}
